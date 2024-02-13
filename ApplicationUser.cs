969bf2ac09b0b6be2523c0d8258954b364a86bd1
manhal11 <71817186+manhal11@users.noreply.github.com>
2024-02-13 20:51:45 +0200

    Add files via upload

---
+++ ApplicationUser.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FastFood.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string Name { get; set; }
+        public string City { get; set; }
+        public string Addresss { get; set; }
+        public string PostalCode { get; set; }
+    }
+}
---
+++ Cart.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FastFood.Models
+{
+    internal class Cart
+    {
+        public int Id { get; set; }
+        public string ItemId { get; set; }
+        public string Item { get; set; }
+        public string ApplicationUserId { get;  set; }
+        public ApplicationUser ApplicationUser { get; set; }
+        [Required,MinLength(1)]
+        public int Coun { get; set; }
+    }
+}
